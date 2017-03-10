df99a996c23afb156ef470c8c6bfa4a6a67c9455
Drew Terry <drewpsterry@gmail.com>
2017-03-11 00:57:34 +0900

    add modal component

--- frontend/app/screens/Home/index.js
+++ frontend/app/screens/Home/index.js
@@ -1,12 +1,43 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router';
+import Modal from 'react-modal';
+import TextEditor from 'components/editor';
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 export default class Home extends Component {
-  //handleSubmit = (e) => {
-    //e.preventDefault();
-    //this.context.router.push({pathname: `/${this._input.value}`});
-  //}
+  constructor() {
+    super();
 
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.refs.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
   render() {
     return (
       <div>
@@ -17,6 +48,26 @@ export default class Home extends Component {
             <p><Link to="quiz"><button type="button" className="btn btn-primary btn-lg">Take the Quiz!</button></Link></p>
           </div>
         </div>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <button onClick={this.closeModal}>close</button>
+
+          <h2 ref="subtitle">Create New Question</h2>
+          <TextEditor />
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
         <div className="container">
           <table className="table">
             <thead>
@@ -49,6 +100,7 @@ export default class Home extends Component {
             </tbody>
           </table>
           <Link to="addquestion"><button type="button" className="btn btn-success">Add a Question</button></Link>
+          <button onClick={this.openModal} type="button" className="btn btn-success">Add a Question</button>
         </div>
       </div>
     );
